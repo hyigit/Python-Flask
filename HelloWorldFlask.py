580513b21caa4c641e3af91594c81ed6caac9d95
hyigit <khyigit@gmail.com>
2018-09-23 00:58:40 +0300

    search ve delete baslandi.

--- HelloWorldFlask.py
+++ HelloWorldFlask.py
@@ -5,5 +5,13 @@ app = Flask('app')
 def hello_world():
   return 'Hello, Alp!'
 
+@app.route('/search')
+def search():
+  return 'Search Page'
+
+@app.route('/delete/<string:id>')
+def deleteId(id):
+  return 'Id: ' + id
+
 app.run(host='0.0.0.0', port=8080)
 
